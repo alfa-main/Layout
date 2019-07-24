1ecaf83b89446636bec57579414f55f3328b7001
metr.2017.rail.com@gmail.com <metr.2017.rail.com@gmail.com>
2019-07-24 11:57:56 +0300

    Layout release

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*document.querySelectorAll('.radio').forEach(function(element){
+	element.onclick = bgChange;
+});
+
+let timer = 0;
+
+function bgChange(){
+	let btnlist = document.querySelectorAll('.btn__list');
+	timer++;
+    if(timer%2 == 0){
+    	for(let i = 0; i < btnlist.length; i++){
+    		if(btnlist[i].checked){
+    	        btnlist.style.color = '#ffffff';
+    		}
+        }
+    }
+    else if(timer%2 != 0){
+        for(let i = 0; i < btnlist.length; i++){
+    	    if(btnlist[i].checked){
+    	        btnlist[i].style.background = '#ffcc00';
+            }
+        }
+
+        let change = document.querySelectorAll('.btn__list p');
+  	    for(let i = 0; i < change.length; i++){
+  	    	if(btnlist[i].checked){
+		        change[i].style.color = '#ba0000';
+	        }
+	    }
+    }
+
+    document.querySelector('.red').style.maxHeight = 'none';
+	document.querySelector('.red').style.minHeight = '340px';
+}*/
+
+
+
+
